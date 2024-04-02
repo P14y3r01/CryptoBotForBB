1ce0b42087de9fbfb8e2d64c6b55b5ffea86e8a4
Aleksandr Golynin <p14y3r011@yandex.ru>
2024-04-02 13:12:32 +0300

    Project base

---
+++ src/test/java/com/example/cryptobotforbb/CryptoBotForBbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptobotforbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoBotForBbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
